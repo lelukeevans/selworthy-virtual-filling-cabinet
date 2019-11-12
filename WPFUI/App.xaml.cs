fb7bba1802581b9710d87d643db0fa6e249dea7c
Luke Evans <levans@selworthy.oak.education>
2019-11-12 14:00:33 +0000

    Completed organized project structure

---
+++ WPFUI/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            var settings = new CefSettings();
+            // Disable GPU in WPF and Offscreen examples until #1634 has been resolved
+            settings.CefCommandLineArgs.Add("disable-gpu", "1");
+            settings.CachePath = "cache";
+
+            Cef.Initialize(settings);
+        }
+    }
+}
